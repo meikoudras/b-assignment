8064d5a971d938dae0f6f4e615286976c2b881d9
Meiko Udras <meikoudras@gmail.com>
2021-11-29 13:26:41 +0200

    add project readme and navigation

---
+++ README.md
@@ -0,0 +1,7 @@
+This project uses node httt-server to serve files
+
+# Set-up
+`npm install`
+
+# Running
+`npm run start`
--- js2/script.js
+++ js2/script.js
@@ -1,11 +1,11 @@
-const countries = {
+var countries = {
     0: 'et',
     1: 'fi',
     2: 'es',
     3: 'de',
     4: 'en',
 };
-const purposes = {
+var purposes = {
     0: 'car',
     1: 'health',
     2: 'business',
@@ -24,11 +24,11 @@ function loadData() {
 }
 
 function createHeader(table, columns) {
-    const head = document.createElement('thead');
-    const row = document.createElement('tr');
+    var head = document.createElement('thead');
+    var row = document.createElement('tr');
     head.appendChild(row);
     columns.forEach(function(column) {
-        const th = document.createElement('th');
+        var th = document.createElement('th');
         th.appendChild(document.createTextNode(column));
         row.appendChild(th);
     })
@@ -40,15 +40,15 @@ function createHeader(table, columns) {
 
 
 function createBody(table) {
-    const body = document.createElement('tbody')
+    var body = document.createElement('tbody')
     table.appendChild(body);
     return body;
 }
 
 function renderRow(body, rowData) {
-    const row = document.createElement('tr');
-    for (const property in rowData) {
-        let value = rowData[property];
+    var row = document.createElement('tr');
+    for (var property in rowData) {
+        var value = rowData[property];
         if(property === 'country') {
             value = countries[value];
         }
@@ -58,7 +58,7 @@ function renderRow(body, rowData) {
         if(property === 'bondora_rating') {
             value = Array(value).join("*")
         }
-        const cell = document.createElement('td');
+        var cell = document.createElement('td');
         cell.appendChild(document.createTextNode(value));
         row.appendChild(cell)
     }
@@ -68,7 +68,7 @@ function renderRow(body, rowData) {
 }
 
 function renderRows(body, data, index, step) {
-    for(let i=index;i<index+step;i++) {
+    for(var i=index;i<index+step;i++) {
         if(data[i]) {
             renderRow(body, data[i]);
         } else {
@@ -78,16 +78,16 @@ function renderRows(body, data, index, step) {
 } 
 
 document.addEventListener("DOMContentLoaded", function() {
-    const table = document.querySelector('#loans');
-    const showMore = document.querySelector('#show-more')
-    const showMoreBottom = document.querySelector('#show-more-bottom')
-    let index = 0;
-    let step = 100;
+    var table = document.querySelector('#loans');
+    var showMore = document.querySelector('#show-more')
+    var showMoreBottom = document.querySelector('#show-more-bottom')
+    var index = 0;
+    var step = 100;
 
     loadData().then(function(rows) {
-        const headerColumns = Object.keys(rows[0]);
-        const head = createHeader(table, headerColumns);
-        const body = createBody(table);
+        var headerColumns = Object.keys(rows[0]);
+        var head = createHeader(table, headerColumns);
+        var body = createBody(table);
         renderRows(body, rows, index, step);
         function show(e){
             index += 100;
@@ -97,6 +97,6 @@ document.addEventListener("DOMContentLoaded", function() {
         showMoreBottom.onclick = show;
     });
 
-    const elements = document.querySelectorAll('thead th');
+    var elements = document.querySelectorAll('thead');
     Stickyfill.add(elements);
   });
